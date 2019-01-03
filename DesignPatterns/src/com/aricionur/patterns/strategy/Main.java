2cff3bf0a76a181aa936dc6009eacd1d17cb5fe3
Onur Arıcı <aricionur@gmail.com>
2019-01-03 19:01:37 +0300

    Strategy Pattern

--- DesignPatterns/src/com/aricionur/patterns/strategy/Main.java
+++ DesignPatterns/src/com/aricionur/patterns/strategy/Main.java
@@ -5,8 +5,14 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		SuperClass superClass = new SubClassA();
-		superClass.performTypeOneBehavior();
-		superClass.performTypeTwoBehavior();
+		SuperClass superClassOne = new SubClassA();
+		superClassOne.performTypeOneBehavior();
+		superClassOne.performTypeTwoBehavior();
+		
+		SuperClass superClassTwo = new SubClassB();
+		superClassTwo.performTypeOneBehavior();
+		superClassTwo.performTypeTwoBehavior();
 	}
+	
+	
 }
